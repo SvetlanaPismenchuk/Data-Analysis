445182b8fbe10bf9ae09d43f9c0d4d9d5ad6dfed
SvetlanaPismenchuk <spismenchuk3085@floridapoly.edu>
2026-02-11 20:58:15 -0500

    Update TreeData.py

--- Data_Analysis/TreeData.py
+++ Data_Analysis/TreeData.py
@@ -1,3 +1,11 @@
+"""
+Extract_data function 
+creates 4 lists.
+Opens the filename.
+Skips headline.
+Appends index to lists.
+Returns the four lists.
+"""
 def extract_data(filename):
     ids = []
     lat = []
@@ -16,18 +24,33 @@ def extract_data(filename):
             height.append(float(words[3]))
 
     return ids, lat, lon, height
-
+'''
+Average_tree_height function takes the sum of the tree heights and divides it by the number of trees.
+'''
 
 def average_tree_height(height):
     return sum(height) / len(height)
 
-
+'''
+Tree_deviation function creates a deviation list
+and appends the deviation using height - average 
+height
+'''
 def tree_deviation(avg, height):
     deviation = []
     for h in height:
         deviation.append(h - avg)
     return deviation
 
+
+'''
+
+Writes a new CSV file.
+Adds a header line.
+Loops through all tree data.
+Writes id, location, height, and deviation to the file.
+
+'''
 
 ids, lat, lon, height = extract_data("tree_data.csv")
 
